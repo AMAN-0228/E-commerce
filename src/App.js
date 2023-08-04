1defee8cd085d90edb158d165724f6e2a74f36a8
Aman <amansahu5051@gmail.com>
2023-08-04 10:46:17 +0530

    inital structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
+import Products from './Pages/Products'
+import Cart from './Pages/Cart'
+import SingleProduct from './Pages/SingleProduct'
+import NotFound from './Pages/NotFound';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Header/>
+      <Route path='/' element={<Home />}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='singleProduct/:id' element={<SingleProduct/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+      <Route path='/products' element={<Products/>}/>
+      <Route path='/cart' element={<Cart/>}/>
+      <Route path='*' element={<NotFound/>}/>
+      <Footer/>
+    </Routes>
   );
 }
 
